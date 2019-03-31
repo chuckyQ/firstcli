0956ef758d0d28e27554f17aa8c6a20b7a2bb8d6
Charles <charles.quinn@comcast.net>
2019-03-30 23:49:18 -0400

    Added an option for object files

--- main.py
+++ main.py
@@ -4,9 +4,10 @@ Usage:
   main.py (-h | --help | --version)
 
 Options:
-  -c, --compiled  Compile source to machine code
-  -h, --help      Show this help screen and exit
-  --version       Give version of tool
+  -o, --object-file=<file>   Give an object file with a name
+  -c, --compiled                 Compile source to machine code
+  -h, --help                     Show this help screen and exit
+  --version                      Give version of tool
 """
 
 __version__ = '0.0.1'
@@ -18,3 +19,9 @@ opts = options = docopt(__doc__, version=__version__)
 if opts['--compiled']:
 
 	print('Compiling to machine code')
+
+if opts['--object-file']:
+
+	obj_file = opts['--object-file']
+	
+	print('Creating object file', obj_file)
